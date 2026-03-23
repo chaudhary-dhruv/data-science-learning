54896ef8f2c8be7ab41781b4e76afffcb8273783
chaudhary-dhruv <dhruv.chaudhary.63513@gmail.com>
2026-03-24 01:25:11 +0530

    Learn about matplotlib

--- Data Visualization/demo_metplotlib.py
+++ Data Visualization/demo_metplotlib.py
@@ -3,11 +3,9 @@ import matplotlib.pyplot as plt
 kohli = [0, 0, 500, 800, 1100, 1300, 1500, 1800, 1900, 2100]
 sehwag = [0, 300, 800, 1200, 1500, 1700, 1600, 1400, 1000, 0]
 
-plt.plot(years, kohli, label="Virat Kohli")
-plt.plot(years, sehwag, label="Virender Sehwag")
-
-plt.xlabel("Year")
-plt.ylabel("Runs Scored")
-plt.title("Performance Comparison")
+plt.plot(years, kohli, linewidth=3, label="Kohli")
+plt.plot(years, sehwag, linewidth=2, label="Sehwag")
 plt.legend()
+plt.grid(True)
+plt.tight_layout()
 plt.show()
